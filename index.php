552ea4cf945bcaccb2f5da7f719bc5be6b0dc14a
danielecarpentiero <carpentierodaniele@gmail.com>
2024-01-08 18:32:51 +0100

    feat: risoluzione

--- index.php
+++ index.php
@@ -4,8 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <form action="second.php" method="get">
+        <h1>Censuratore automatico</h1>
+        <h2>Scrivi qualcosa nel paragrafo</h2>
+        <textarea name="paragraph" id="paragraph" cols="50" ></textarea>
+        <h2>Parola da censurare</h2>
+        <input type="text" name="censor" id="censor">
+        <input type="submit" value="Invia">
+    </form>
 </body>
 </html>
--- second.php
+++ second.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Censored paragraph</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div>
+        
+    <?php 
+        $paragraph = $_GET['paragraph'];
+        $censored_word = $_GET['censor'];
+
+        echo "<h2>Paragrafo originale:</h2>";
+        echo "$paragraph";
+        echo "<p>Lunghezza: " . strlen($paragraph) . "</p>";
+
+        $censored_paragraph = str_ireplace($censored_word, "***", $paragraph);
+
+        echo "<h2>Paragrafo modificato:</h2>";
+        echo "$censored_paragraph";
+        echo "<p>Lunghezza: " . strlen($censored_paragraph) . "</p>";
+    ?>
+    </div>
+
 </body>
 </html>
